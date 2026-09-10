f17b0162fc9398438d520e5eea990cd75684574e
Audrey <brooksaudrey95@gmail.com>
2026-09-10 12:55:19 -0500

    Updates
    Cleaned files up, fixed styling and sizing on navigation bar, fixed education section carousel and fixed it to handle other screen sizes, fixed footer and contact section to handle other screen sizes
    
    Found Bug: skills carousel overshoots cards after a bit in smaller screen sizes...

--- javascript/edu-carousel.js
+++ javascript/edu-carousel.js
@@ -20,33 +20,28 @@ const downButton = document.querySelector(".edu-next");
 
 // Global Variables:
 let edu_currentIndex = 0;               // index of card we're focused on
-let edu_maxIndex = cards.length - 1;    // Number of cards we have
+let edu_maxIndex = edu_cards.length - 1;    // Number of cards we have
 
 
 function updateEduCarousel() {
     // Calculate movement:
     // how far = current card # * (card Height + gap between cards)
     const cardHeight = edu_cards[0].offsetHeight;
-    const gap = 50; 
-    const movement = edu_currentIndex * (cardHeight + gap);
+    const edu_gap = 44; 
+    const edu_movement = edu_currentIndex * (cardHeight + edu_gap);
 
     // Conduct movement
-    edu_track.style.transform = `translateY(-${movement}px)`;
-    edu_track.style.transform = `translateY(-450px)`;
+    edu_track.style.transform = `translateY(-${edu_movement}px)`;
 
-    //update buttons
-    updateButtons();
-}
-
-function updateEduButtons() {
     // Previous Button Updater:
     if (edu_currentIndex <= 0)    {  upButton.disabled = true;  }
-    else                         {  upButton.disabled = false; }
+    else                          {  upButton.disabled = false; }
 
     // Next Button Updater:
-    if (edu_currentIndex > edu_maxIndex) {  downButton.disabled = true;  }
+    if (edu_currentIndex >= edu_maxIndex) {  downButton.disabled = true;  }
     else                                 {  downButton.disabled = false; }
 }
+
 
 /** */
 downButton.addEventListener("click", () => {
@@ -54,10 +49,10 @@ downButton.addEventListener("click", () => {
     updateEduCarousel();
 });
 
-
 upButton.addEventListener("click", () => {
     edu_currentIndex--;
     updateEduCarousel();
 });
+
 
 updateEduCarousel();
